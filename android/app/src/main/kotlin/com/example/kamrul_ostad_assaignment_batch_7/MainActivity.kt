2adf37f554b790f9fdc7d42013d8ad7be07579ec
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2024-06-20 02:38:20 +0600

    Assaignment Done

---
+++ android/app/src/main/kotlin/com/example/kamrul_ostad_assaignment_batch_7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kamrul_ostad_assaignment_batch_7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'carClass.dart';
+
+void main(){
+  Car toyota = Car(brand: 'Toyota', model:'Camry', year: 2015);
+  Car honda = Car(brand: 'Honda', model:'Accord', year: 2018);
+  Car ford = Car(brand: 'Ford', model:'Mustang', year: 2020);
+
+  toyota.drive(1000);
+  honda.drive(1500);
+  ford.drive(2000);
+
+  print('Car name : ${toyota.getBrand()}, Model name : ${toyota.getModel()}, Year is : ${toyota.getYear()}, Miles Driven : ${toyota.getMilesDriven()}, Car Age : ${toyota.getAge()}');
+  print('Car name : ${honda.getBrand()}, Model name : ${honda.getModel()}, Year is : ${honda.getYear()} ,  Miles Driven : ${honda.getMilesDriven()}, Car Age : ${honda.getAge()}');
+  print('Car name : ${ford.getBrand()}, Model name : ${ford.getModel()}, Year is : ${ford.getYear()},  Miles Driven : ${ford.getMilesDriven()}, Car Age : ${ford.getAge()}');
+
+  print('No of Car is : ${Car.numberOfCars}');
+}
